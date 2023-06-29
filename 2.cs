5ff071795c013759f4e5a3ac317be51029daaf96
Kevin <chx100384@student.chorzow.wsb.pl>
2023-06-30 00:36:46 +0300

    Create 2.cs

---
+++ 2.cs
@@ -0,0 +1,22 @@
+struct Student
+{
+    public string Imię;
+    public string Nazwisko;
+    public int NumerIndeksu;
+    public string Kierunek;
+}
+
+struct Przedmiot
+{
+    public string Nazwa;
+    public string Kod;
+    public int ECTS;
+}
+
+struct NauczycielAkademicki
+{
+    public string Imię;
+    public string Nazwisko;
+    public string StopieńNaukowy;
+    public string Katedra;
+}
