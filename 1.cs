e2b53026695f29f1b760d8302944725c02d8fd6b
Kevin <chx100384@student.chorzow.wsb.pl>
2023-06-30 00:35:45 +0300

    Create 1.cs

---
+++ 1.cs
@@ -0,0 +1,41 @@
+// Dni tygodnia
+enum DniTygodnia
+{
+    Poniedziałek,
+    Wtorek,
+    Środa,
+    Czwartek,
+    Piątek,
+    Sobota,
+    Niedziela
+}
+
+// Etapy prania w pralce automatycznej
+enum EtapyPrania
+{
+    Wypełnianie,
+    Płukanie,
+    Wirowanie,
+    Suszenie
+}
+
+// Posiłki w ciągu dnia
+enum Posiłki
+{
+    Śniadanie,
+    DrugieŚniadanie,
+    Obiad,
+    Podwieczorek,
+    Kolacja
+}
+
+// Biurka szachowe
+enum BierkiSzachowe
+{
+    Król,
+    Hetman,
+    Wieża,
+    Goniec,
+    Skoczek,
+    Pion
+}
